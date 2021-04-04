d148f294bf143ad0df7dfecc96da3bdfa40aae32
Aditya Ardiansyah <03adityaardiansyah1999@gmail.com>
2021-04-04 22:45:04 +0900

    update tombol slider

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,18 +1,41 @@
-$('.owl-carousel').owlCarousel({
-    rtl:false,
-    dots: false,
-    autoWidth:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:10
+$(document).ready(function(){
+    $('#owl-one').owlCarousel({
+        rtl:false,
+        nav:true,
+        navText: ["<div class='nav-button owl-prev'><img src='../assets/WEB/ICON/ICON WEB24.svg'></div>", "<div class='nav-button owl-next'><img src='../assets/WEB/ICON/ICON WEB14.svg'></div>"],
+        dots: false,
+        autoWidth:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:10
+            }
         }
-    }
+    })
+    $('#owl-two').owlCarousel({
+        rtl:false,
+        nav:true,
+        navText: ["<div class='nav-button owl-prev'><img src='../assets/WEB/ICON/ICON WEB24.svg'></div>", "<div class='nav-button owl-next'><img src='../assets/WEB/ICON/ICON WEB14.svg'></div>"],
+        dots: false,
+        autoWidth:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:10
+            }
+        }
+    })
+    $('.owl-nav').removeClass('disabled');
 })
 
 function tab(evt, choice) {
