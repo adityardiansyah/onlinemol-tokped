3ac2fad27fe9876e2fa7372720cb830f7637c050
Aditya Ardiansyah <03adityaardiansyah1999@gmail.com>
2021-04-24 01:09:57 +0900

    update nav & update detail

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -57,32 +57,10 @@ $(document).ready(function(){
     $('.owl-nav').removeClass('disabled');
 
     $('.centers').slick({
-        centerMode: true,
-        centerPadding: '60px',
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-            {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-            }
-        ]
+        autoplaySpeed: 5000
     });
 })
 
