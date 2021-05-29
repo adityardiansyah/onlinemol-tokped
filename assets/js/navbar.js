157100454591603da0c39c1d557e2ca684a04704
Aditya Ardiansyah <03adityaardiansyah1999@gmail.com>
2021-05-29 13:01:46 +0700

    update perapihan

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,12 @@
 $('.dropdown-custom').hover(function(){
     $('.bg-content-dropdown').css("opacity", "0.6")
+    $('.bg-content-dropdown').css("display", "block")
 }, function(){
     $('.bg-content-dropdown').css("opacity", "0")
+    $('.bg-content-dropdown').css("display", "none")
 })
+
+$('.tab-label').click(function(){
+    $('.tab-label').removeClass('active');
+    $(this).addClass('active');
+});
